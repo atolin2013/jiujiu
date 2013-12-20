f2b1aaa1c26d4cc3f1cf6e691fe9b8977b79f572
unknown <wenyu.lin@linwenyu-6230.autonavi.com>
2013-12-20 14:46:24 +0800

    'init'

---
+++ g2f/cfg.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# --*-- coding:gbk --*--
+
+import os
+import sys
+import ConfigParser
+import logging
+
+logger = logging.getLogger("")
+
+def from_file(_path, _section):
+    if not os.path.isfile(_path):
+        return None
+    _items = {}
+    cfg = ConfigParser.ConfigParser()
+    try:
+	cfg.read(_path)
+    except ConfigParser.Error:
+	logger.error(ConfigParser.Error.message)
+	return None
+    if cfg.has_section(_section):
+        for k,v in cfg.items(_section):
+            _items[k] = v
+    return _items
+
+def to_file(_path, _section, _items):
+    config = ConfigParser.ConfigParser()
+    config.add_section(_section.lower())
+    for k,v in _items.items():
+        config.set(_section.lower(), k, v)
+
+    with open(_path, 'wb') as configfile:
+        config.write(configfile)
+
+if __name__=="__main__":
+    print 'unsupport main function.'
+
+
